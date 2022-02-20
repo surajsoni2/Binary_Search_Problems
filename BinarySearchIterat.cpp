e96eada9265705a86b11e877051e51fe694422e6
Suraj Soni <64774906+surajsoni2@users.noreply.github.com>
2022-02-20 16:40:28 +0530

    Update BinarySearchIterat.cpp

--- BinarySearchIterat.cpp
+++ BinarySearchIterat.cpp
@@ -1,29 +1,39 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int IBS(int arr[],int n,int key){
-    
-    int s=0;
-    int e=n-1;
-    while(s<=e){
-    int mid = (s+e)/2;
-    
-        if(key==arr[mid])
-            return mid;
-        else if(key>arr[mid])
-            s=mid+1;
-        else
-            e=mid-1;
+int IBS(int arr[], int n, int key)
+{
+    int s = 0;
+    int e = n - 1;
+    while (s <= e)
+    {
+        int mid = (s + e) / 2;
+        if (key == arr[mid]) return mid; 
+        else if (key > arr[mid]) s = mid + 1;
+        else e = mid - 1;    
     }
     return -1;
-    
 }
 
-int main(){
-    int arr[] = {1};
-    cout<<"enter elemnt from 1-15 tp be search";
+int main()
+{
+    int n;
+    cout << "enter the size of the array";
+    cin >> n;
+    int arr[n];
+
+    // Taking input elements of array
+    cout << "enter elements in sorted order"<<endl;
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    // key element to be searched
     int key = 0;
-    cin>>key;
-    int x=IBS(arr,1,key);
-    cout<<x;
+    cout<<"enter the key to search"<<endl;
+    cin >> key;
+
+    int x = IBS(arr, n, key);
+    cout << x;
+
+    return 0;
 }
